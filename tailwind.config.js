95ad12407895b2174232661aeba839de2ca91306
Ihsansrc <ihsan100905@gmail.com>
2023-09-03 14:52:12 +0700

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [".//**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      fontFamily: {
+        inter: "Inter",
+      },
+    },
   },
   plugins: [],
 };
