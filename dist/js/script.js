53c3b2d9c3b6495d8c72bddbe1473901dec3f3cd
Ihsansrc <ihsan100905@gmail.com>
2023-09-07 07:00:41 +0700

    menambahkan dark mode dan perubahan pada nav

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,19 @@
 // navbar fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
+  const toTop = document.querySelector("#top");
   const fixedNav = header.offsetTop;
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
+
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
 
@@ -18,4 +25,24 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// klick di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
 });
