96bb2088c91ed95cde747688053d9dda229d8ae6
Ihsansrc <ihsan100905@gmail.com>
2023-09-07 07:47:24 +0700

    menambahkan berupa stabilitas toggle dark biar sesuai dengan local storage

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,7 +42,21 @@ const html = document.querySelector("html");
 darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
     html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
+
+// pindahkan posisi toggle sesuai mode dark .. jd misal local na pilih dark dan sama dark maka dhecked nya akan dark terus karena true
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
