325cf66b4ba7c2ac896f5aa3d09652689d4d6c75
Ihsansrc <ihsan100905@gmail.com>
2023-10-06 16:20:06 +0700

    buat pop up dan kalkulator

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,7 +14,7 @@ window.onscroll = function () {
 
     toTop.classList.add("hidden");
     toTop.classList.remove("flex");
-  }
+  } // kurawal di 17
 };
 
 // hamburger
@@ -60,3 +60,93 @@ if (
 } else {
   darkToggle.checked = false;
 }
+
+const close = document.querySelectorAll("#close");
+close.forEach(function (e) {
+  e.addEventListener("click", function (close) {
+    close.target.parentElement.style.display = "none";
+    console.log(e);
+    console.log(close.target);
+  });
+});
+
+const hati = document.querySelectorAll("#hati");
+hati.forEach(function (e) {
+  let sudahPencet = false;
+  e.addEventListener("click", function () {
+    if (sudahPencet) {
+      // awalnya false tapi ketika di klik menjadi true
+      e.setAttribute("src", "gambar/heart.svg");
+    } else {
+      e.setAttribute("src", "gambar/twitter.svg");
+    }
+
+    sudahPencet = !sudahPencet; // jimat isClicked = !isClicked digunakan untuk mengubah nilai isClicked dari false menjadi true atau dari true menjadi false, tergantung pada nilai sebelumnya.
+    console.log(e.target);
+  });
+});
+
+// kalkulator
+
+const screen = document.getElementById("screen");
+
+function appendToScreen(value) {
+  screen.value += value;
+}
+
+function clearScreen() {
+  screen.value = "";
+}
+
+function del() {
+  screen.value = screen.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    screen.value = eval(screen.value);
+  } catch (error) {
+    screen.value = "Error";
+  }
+}
+
+const coba = document.querySelector(".coba");
+function tombol() {
+  for (var i = 9; i >= 1; i--) {
+    const tombol = document.createElement("button");
+    tombol.textContent = i;
+    tombol.setAttribute(
+      "class",
+      "w-10 h-10 text-center flex justify-center items-center rounded-md bg-white"
+    );
+    tombol.setAttribute("onclick", `appendToScreen('${i}')`);
+    coba.appendChild(tombol);
+  }
+}
+
+tombol();
+
+const kosong = document.querySelector(".coba");
+function nol() {
+  for (var i = 3; i >= 1; i--) {
+    const nol = document.createElement("button");
+    nol.textContent = i;
+    nol.setAttribute(
+      "class",
+      "w-10 h-10 text-center flex justify-center items-center rounded-md bg-white"
+    );
+
+    if (i == 2) {
+      nol.textContent = "0";
+      nol.setAttribute("onclick", "appendToScreen('0')");
+    } else {
+      nol.textContent = "";
+
+      nol.setAttribute("onclick", "appendToScreen('')");
+    }
+
+    kosong.appendChild(nol);
+  }
+}
+
+nol();
